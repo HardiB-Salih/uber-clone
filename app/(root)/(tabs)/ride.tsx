14f3df40155fd5ff15d064527c3773bc412466df
HardiB-Salih <hardib.salih@gmail.com>
2024-08-27 23:44:15 +0300

    initial commit

---
+++ app/(root)/(tabs)/ride.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Ride() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center text-blue-600">
+      <Text className="text-2xl">Ride</Text>
+    </SafeAreaView>
+  );
+}
